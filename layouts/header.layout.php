319ae88d928b79f3f5d41565fd5bfce2c5f3f7ee
gabbycam98 <gabcamino1298@gmail.com>
2025-06-23 07:05:23 +0800

    feat: add header and footer layout files for improved structure

---
+++ layouts/header.layout.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="../../assets/css/header.css">
+</head>
+
+<body>
+    <!-- header section -->
+    <header class="header">
+        <div class="left-section">
+            <a href="../../index.php">
+                <img src="../../assets/img/travelez-green.png" alt="travelEZ logo">
+            </a>
+        </div>
+        <div class="middle-section">
+        </div>
+        <div class="right-section">
+            <button class="nav-header">About Us</button>
+        </div>
+    </header>
+</body>
+
+</html>
