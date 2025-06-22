2d4af53c318de0c0fcc4a5e5448d3d642bfe930f
gabbycam98 <gabcamino1298@gmail.com>
2025-06-22 22:42:47 +0800

    feat: added landing page

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TravelEz - Know Where to Go, When to Go!</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+
+
+</head>
+
+<body>
+
+    <!-- header section -->
+    <header class="header">
+        <div class="left-section">
+            <a href="../../index.php">
+                <img src="../../assets/img/travelez-green.png" alt="travelEZ logo">
+            </a>
+        </div>
+        <div class="middle-section">
+        </div>
+        <div class="right-section">
+            <button class="nav-header">About Us</button>
+        </div>
+    </header>
+
+
+    <main>
+        <section class="landing-page">
+            <div class="description-container">
+                <div class="travelez-description">
+                    <h1>Welcome to TravelEZ!</h1>
+                    <p class="description">TravelEZ is a user-friendly website that provides a comprehensive timetable
+                        of bus routes across the Philippines. Whether you're planning a trip from Manila to North Luzon
+                        or other key destinations, TravelEZ helps you explore available routes offered by various bus
+                        companies!</p>
+                </div>
+                <div class="button-container">
+                    <a href="pages/timetable/index.php">
+                        <button class="btn-1">Let's Go!</button>
+                    </a>
+                </div>
+            </div>
+
+
+        </section>
+
+    </main>
+
+    <footer class="footer">
+        <div class="footer-container">
+            <div class="footer-image">
+                <img src="../assets/img/travelez-green.png" alt="TravelEZ Logo">
+            </div>
+            <div class="copyright-info">
+                <p>© 2025 TravelEZ Philippines, All Rights Reserved.</p>
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
