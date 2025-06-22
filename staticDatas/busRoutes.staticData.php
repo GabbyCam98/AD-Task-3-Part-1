9a43c021a4116e97872359407eef6bebdd0e8900
gabbycam98 <gabcamino1298@gmail.com>
2025-06-22 22:45:43 +0800

    feat: add detailed bus schedules for multiple transport services

---
+++ staticDatas/busRoutes.staticData.php
@@ -0,0 +1,167 @@
+<?php
+$busRoutes = [
+    "Victory Liner" => [
+        [
+            "origin" => "Kamias",
+            "destination" => "Tuguegarao",
+            "schedules" => [
+                ["time" => "9:00 AM", "class" => "Regular Airconditioned"],
+                ["time" => "8:00 PM", "class" => "First Class"],
+                ["time" => "8:45 PM", "class" => "Royal Class (SLEEPER)"]
+            ]
+        ],
+        [
+            "origin" => "Tuguegarao",
+            "destination" => "Cubao/Sampaloc",
+            "schedules" => [
+                ["time" => "8:00 AM", "class" => "Regular Airconditioned"],
+                ["time" => "12:45 PM", "class" => "First Class"],
+                ["time" => "9:00 PM", "class" => "Royal Class (SLEEPER)"]
+            ]
+        ],
+        [
+            "origin" => "Cubao",
+            "destination" => "Baguio",
+            "schedules" => [
+                ["time" => "7:00 AM", "class" => "Regular Airconditioned"],
+                ["time" => "10:00 AM", "class" => "First Class"],
+                ["time" => "10:00 PM", "class" => "Sleeper"]
+            ]
+        ],
+        [
+            "origin" => "Baguio",
+            "destination" => "Cubao",
+            "schedules" => [
+                ["time" => "7:30 AM", "class" => "Regular Airconditioned"],
+                ["time" => "8:00 PM", "class" => "First Class"],
+                ["time" => "11:00 PM", "class" => "Sleeper"]
+            ]
+        ],
+        [
+            "origin" => "Sampaloc",
+            "destination" => "Cauayan",
+            "schedules" => [
+                ["time" => "9:00 AM", "class" => "Regular Airconditioned"],
+                ["time" => "4:00 PM", "class" => "First Class"],
+                ["time" => "8:30 PM", "class" => "Regular Airconditioned"]
+            ]
+        ],
+        [
+            "origin" => "Cauayan",
+            "destination" => "Cubao/Sampaloc",
+            "schedules" => [
+                ["time" => "11:00 AM", "class" => "Regular Airconditioned"],
+                ["time" => "5:00 PM", "class" => "First Class"],
+                ["time" => "7:45 PM", "class" => "Regular Airconditioned"]
+            ]
+        ],
+    ],
+    "Genesis Transport Service" => [
+        [
+            "origin" => "Cubao",
+            "destination" => "Mariveles",
+            "schedules" => [
+                ["time" => "9:00 AM", "class" => "Regular Airconditioned"],
+                ["time" => "11:00 AM", "class" => "Regular Airconditioned"],
+                ["time" => "3:00 PM", "class" => "Regular Airconditioned"]
+            ],
+        ],
+        [
+            "origin" => "Mariveles",
+            "destination" => "Cubao",
+            "schedules" => [
+                ["time" => "10:00 AM", "class" => "Regular Airconditioned"],
+                ["time" => "12:00 PM", "class" => "Regular Airconditioned"],
+                ["time" => "3:00 PM", "class" => "Regular Airconditioned"]
+            ],
+        ],
+        [
+            "origin" => "Cubao",
+            "destination" => "Baguio",
+            "schedules" => [
+                ["time" => "8:00 AM", "class" => "Regular Airconditioned"],
+                ["time" => "10:00 AM", "class" => "Joybus Executive Coach"],
+                ["time" => "12:00 PM", "class" => "Joybus Executive Coach"],
+                ["time" => "4:00 PM", "class" => "Regular Airconditioned"]
+            ],
+        ],
+        [
+            "origin" => "Baguio",
+            "destination" => "Clark",
+            "schedules" => [
+                ["time" => "8:30 AM", "class" => "Joybus Executive Coach"],
+                ["time" => "11:20 AM", "class" => "Joybus Executive Coach"],
+                ["time" => "1:00 PM", "class" => "Joybus Executive Coach"],
+                ["time" => "6:00 PM", "class" => "Joybus Executive Coach"]
+            ]
+        ]
+    ],
+    "Partas Trans" => [
+        [
+            "origin" => "Cubao",
+            "destination" => "Vigan",
+            "schedules" => [
+                ["time" => "9:00 AM", "class" => "Regular Airconditioned"],
+                ["time" => "11:00 AM", "class" => "Luxury Class 2x1"],
+                ["time" => "5:00 PM", "class" => "Regular Airconditioned"],
+                ["time" => "11:00 PM", "class" => "Luxury Class 2x1"]
+            ],
+        ],
+        [
+            "origin" => "Vigan",
+            "destination" => "Cubao",
+            "schedules" => [
+                ["time" => "8:00 AM", "class" => "Regular Airconditioned"],
+                ["time" => "10:00 AM", "class" => "Luxury Class 2x1"],
+                ["time" => "2:00 PM", "class" => "Regular Airconditioned"],
+                ["time" => "8:00 PM", "class" => "Luxury Class 2x1"]
+            ],
+        ],
+        [
+            "origin" => "Cubao",
+            "destination" => "Laoag",
+            "schedules" => [
+                ["time" => "6:00 AM", "class" => "Luxury Class 2x1"],
+                ["time" => "9:00 AM", "class" => "Regular Airconditioned"],
+                ["time" => "12:00 PM", "class" => "Luxury Class 2x1"],
+                ["time" => "2:00 PM", "class" => "Luxury Class 2x1"],
+                ["time" => "5:00 PM", "class" => "Regular Airconditioned"],
+                ["time" => "9:00 PM", "class" => "Luxury Class 2x1"],
+            ]
+        ],
+        [
+            "origin" => "Laoag",
+            "destination" => "Cubao",
+            "schedules" => [
+                ["time" => "6:30 AM", "class" => "Luxury Class 2x1"],
+                ["time" => "8:45 AM", "class" => "Regular Airconditioned"],
+                ["time" => "11:30 PM", "class" => "Luxury Class 2x1"],
+                ["time" => "1:00 PM", "class" => "Luxury Class 2x1"],
+                ["time" => "4:00 PM", "class" => "Regular Airconditioned"],
+                ["time" => "10:00 PM", "class" => "Luxury Class 2x1"],
+            ]
+        ],
+        [
+            "origin" => "Pasay",
+            "destination" => "Tuguegarao",
+            "schedules" => [
+                ["time" => "4:00 PM", "class" => "Regular Airconditioned"],
+                ["time" => "6:00 PM", "class" => "Regular Airconditioned"],
+                ["time" => "8:00 PM", "class" => "Regular Airconditioned"]
+            ]
+        ],
+        [
+            "origin" => "Tuguegarao",
+            "destination" => "Pasay",
+            "schedules" => [
+                ["time" => "4:00 PM", "class" => "Regular Airconditioned"],
+                ["time" => "6:00 PM", "class" => "Regular Airconditioned"],
+                ["time" => "8:00 PM", "class" => "Regular Airconditioned"]
+            ]
+        ]
+    ]
+];
+
+
+
+?>
