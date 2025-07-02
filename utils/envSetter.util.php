d4251746800acb4ca32cb251859d3265f802f3da
gabbycam98 <gabcamino1298@gmail.com>
2025-07-02 18:09:03 +0800

    feat: add envSetter utility for environment variable management

---
+++ utils/envSetter.util.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once BASE_PATH . '/bootstrap.php';
+require_once BASE_PATH . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
+$dotenv->load();
+
+// Distribute the data using array key
+$typeConfig = [
+    'key' => $_ENV['ENV_NAME'],
+];
