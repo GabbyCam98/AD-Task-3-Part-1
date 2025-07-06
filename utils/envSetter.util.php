d803714c546a91e4fb0d643f83db9d7ebfbb2f5b
gabbycam98 <gabcamino1298@gmail.com>
2025-07-06 15:05:02 +0800

    refactor: reorganize environment variable distribution for database configuration

--- utils/envSetter.util.php
+++ utils/envSetter.util.php
@@ -6,7 +6,12 @@ require_once BASE_PATH . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
 $dotenv->load();
 
-// Distribute the data using array key
-$typeConfig = [
-    'key' => $_ENV['ENV_NAME'],
+$databases = [
+    'pgHost' => $_ENV['PG_HOST'],
+    'pgPort' => $_ENV['PG_PORT'],
+    'pgDB' => $_ENV['PG_DB'],
+    'pgUser' => $_ENV['PG_USER'],
+    'pgPassword' => $_ENV['PG_PASS'],
+    'mURI' => $_ENV['MONGO_URI'],
+    'mDB' => $_ENV['MONGO_DB'],
 ];
