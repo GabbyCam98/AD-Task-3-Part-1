61f1491325df46ebfae7aeafc8763ca868ac9b5b
gabbycam98 <gabcamino1298@gmail.com>
2025-07-06 13:45:43 +0800

    refactor: update login page structure and improve include statements

--- pages/loginPage/index.php
+++ pages/loginPage/index.php
@@ -21,6 +21,8 @@ $currentTime = strtotime(date("H:i"));
 
     <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="stylesheet" href="/assets/css/header.css">
+    <link rel="stylesheet" href="/assets/css/sections.css">
+
 
 
 </head>
@@ -28,86 +30,56 @@ $currentTime = strtotime(date("H:i"));
 <body>
 
     <?php
-    require_once("/layouts/header.layout.php");
+    include_once("../../layouts/header.layout.php");
     ?>
 
     <!-- Timetable section -->
-    <div class="content">
+    <main>
 
-        <div class="error-handler">
+
+
+        <div class="container login">
+
+            <!-- <img class="travelez-logo" src="/assets/img/travelez-purple.png" alt=""> -->
+            <h1>Log In</h1>
+
+
+            <form action="../../pages/timetable/timetable.php" method="post">
+
+                <label for="username">
+                    <p>Username: </p>
+                </label>
+                <input type="text" id="username" name="username" placeholder="Enter Username" required>
+                <label for="password">
+                    <p>Password: </p>
+                </label>
+                <input type="text" id="password" name="password" placeholder="Enter Password" required>
+
+                <div class="action-buttons">
+                    <button type="submit" class="btn-1 submit">Log In</button>
+
+                </div>
+
+
+            </form>
+
+
             <?php
-            include('../../handlers/postgreChecker.handler.php');
+            require_once('../../handlers/postgreChecker.handler.php');
+            require_once('../../handlers/mongodbChecker.handler.php');
             ?>
-            <?php
-            include('../../handlers/mongodbChecker.handler.php');
-            ?>
+
         </div>
 
-        <?php
-
-        foreach ($busRoutes as $company => $routes) {
-            echo "<div class='schedule-container'>";
-
-            echo "<div class='company-container'>";
-            if (isset($busLogos[$company])) {
-                $logo = $busLogos[$company];
-                echo "<img class='company-logo' src='$logo'>";
-            } else {
-                echo "<h1 style='display: inline-block; vertical-align: middle;'>$company</h1>";
-            }
-            echo "</div>";
-
-            /* Route, sched, and class table */
-            echo "<div class='company-grid'>";
-
-            foreach ($routes as $route) {
-                echo "<div class='route'>";
-                echo "<h2>" . $route['origin'] . " → " . $route['destination'] . "</h2>";
-                echo "<div class='schedule'>";
-                echo "</div>";
-                echo "<table cellpadding='5' cellspacing='0'>";
-                echo "<tr><th>Time</th><th>Bus Class</th><th>Status</th></tr>";
-
-                /* Schedule and Bus Class */
-                for ($i = 0; $i < count($route['schedules']); $i++) {
-                    $trip = $route['schedules'][$i];
-
-                    echo "<tr>";
-                    echo "<td class='time'>" . $trip['time'] . "</td>";
-                    echo "<td class='class'>" . $trip['class'] . "</td>";
-
-                    /* Trip status */
-                    $currentTime = strtotime("now");
-                    $tripSched = strtotime(date("Y-m-d") . " " . $trip['time']);
-                    $boardingTime = ($tripSched - $currentTime) / 60;
-
-                    if ($tripSched < $currentTime) {
-                        echo "<td class='departed' style='text-align: right' >Departed</td>";
-                    } elseif ($boardingTime <= 45 && $boardingTime >= 30) {
-                        echo "<td class='boarding' style='text-align: right'>Boarding</td>";
-                    } else {
-                        echo "<td class='not-departed' style='text-align: right' >Scheduled</td>";
-                    }
-                    echo "</tr>";
-                }
-                echo "</table><br>";
-                echo "</div>";
-            }
-            echo "</div>";
-            echo "</div>";
-        }
-        ?>
-
-        <a href="../../index.php">
-            <button class="btn-2">Return to Home</button>
-        </a>
+    </main>
 
 
-    </div>;
+
+
 
     <!-- footer section -->
     <?php
-    require_once("/layouts/footer.layout.php");
+    include_once("../../layouts/footer.layout.php");
     ?>
 
 
