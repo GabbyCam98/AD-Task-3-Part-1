0c375eca8790ff46025148bd4f7443129fb39b16
gabbycam98 <gabcamino1298@gmail.com>
2025-07-06 09:37:28 +0800

    refactor: remove redundant id column from project_users table

--- database/project_users.models.sql
+++ database/project_users.models.sql
@@ -1,5 +1,4 @@
 CREATE TABLE IF NOT EXISTS project_users (
-    id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
     project_id INTEGER NOT NULL REFERENCES projects (id),
     user_id INTEGER NOT NULL REFERENCES users (id),
     PRIMARY KEY (project_id, user_id)
