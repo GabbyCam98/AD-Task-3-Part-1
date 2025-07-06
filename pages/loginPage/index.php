3f61a002bfa18afd5af31131ccd8c423003f0382
gabbycam98 <gabcamino1298@gmail.com>
2025-07-06 15:05:12 +0800

    fix: update page title and add favicon for login page

--- pages/loginPage/index.php
+++ pages/loginPage/index.php
@@ -12,7 +12,8 @@ $currentTime = strtotime(date("H:i"));
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Log In</title>
+    <link rel="icon" href="../../assets/img/travelez-icon-green.png">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
