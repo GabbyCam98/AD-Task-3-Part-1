e86b441236d0a56ffc86bbde88987378a4bf0a0b
gabbycam98 <gabcamino1298@gmail.com>
2025-07-07 22:22:10 +0800

    feat(about): create About Us page with header and footer includes

---
+++ pages/aboutUs/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TravelEZ - About Us</title>
+    <link rel="icon" href="../../assets/img/travelez-icon-green.png">
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/sections.css">
+
+</head>
+
+<body>
+
+    <?php
+    include_once("../../layouts/header.layout.php");
+    ?>
+
+    <main>
+
+        <div class="about-section">
+            <h1>Our Story</h1>
+        </div>
+
+    </main>
+
+    <?php
+    include_once("../../layouts/footer.layout.php");
+    ?>
+
+</body>
+
+</html>
