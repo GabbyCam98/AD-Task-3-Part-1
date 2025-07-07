e475ecd4ec13bd8ea503da5f8510a0c54a445767
gabbycam98 <gabcamino1298@gmail.com>
2025-07-07 22:21:11 +0800

    fix(footer): update logo image source to the correct white version

--- layouts/footer.layout.php
+++ layouts/footer.layout.php
@@ -15,7 +15,7 @@
     <footer class="footer">
         <div class="footer-container">
             <div class="footer-image">
-                <img src="../../assets/img/travelez-green.png" alt="TravelEZ Logo">
+                <img src="../../assets/img/travelez-white.png" alt="TravelEZ Logo">
             </div>
             <div class="copyright-info">
                 <p>© 2025 TravelEZ Philippines, All Rights Reserved.</p>
