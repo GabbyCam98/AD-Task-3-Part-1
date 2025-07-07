ffadf22be6d377dfe3ad6fe118273e05b7e0736d
gabbycam98 <gabcamino1298@gmail.com>
2025-07-07 22:21:41 +0800

    feat(header): add image button hover effects for improved interactivity

---
+++ assets/js/header.js
@@ -0,0 +1,9 @@
+const imgBtn = document.getElementById("img-btn");
+
+imgBtn.addEventListener("mouseover", () => {
+  imgBtn.src = "../../assets/img/travelez-green.png";
+});
+
+imgBtn.addEventListener("mouseout", () => {
+  imgBtn.src = "../../assets/img/travelez-white.png";
+});
