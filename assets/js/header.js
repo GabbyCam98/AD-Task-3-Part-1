7d3c38441da40bb3c54af1dd2b31aa53fa893c14
gabbycam98 <gabcamino1298@gmail.com>
2025-07-14 15:22:41 +0800

    fix: wrap imgBtn event listeners in DOMContentLoaded for proper initialization

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,9 +1,11 @@
-const imgBtn = document.getElementById("img-btn");
+document.addEventListener("DOMContentLoaded", () => {
+  const imgBtn = document.getElementById("img-btn");
 
-imgBtn.addEventListener("mouseover", () => {
-  imgBtn.src = "../../assets/img/travelez-green.png";
-});
+  imgBtn.addEventListener("mouseover", () => {
+    imgBtn.src = "/assets/img/travelez-green.png";
+  });
 
-imgBtn.addEventListener("mouseout", () => {
-  imgBtn.src = "../../assets/img/travelez-white.png";
+  imgBtn.addEventListener("mouseout", () => {
+    imgBtn.src = "/assets/img/travelez-white.png";
+  });
 });
