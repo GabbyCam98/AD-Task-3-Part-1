30dd6fa71a923f50cd594a4bd56624c65166307c
gabbycam98 <gabcamino1298@gmail.com>
2025-07-14 16:11:55 +0800

    refactor: changed routes

--- bootstrap.php
+++ bootstrap.php
@@ -1,13 +1,19 @@
 <?php
-define('BASE_PATH', __DIR__);
-define('HANDLERS_PATH', BASE_PATH . '/handlers');
-define('UTILS_PATH', BASE_PATH . '/utils');
-define('DATABASE_PATH', BASE_PATH . '/database');
-define('DUMMIES_PATH', BASE_PATH . '/staticDatas/dummies');
-define('TEMPLATES_PATH', BASE_PATH . '/components/templates');
-define('STATICDATAS_PATH', BASE_PATH . '/staticDatas');
-define('LAYOUTS_PATH', BASE_PATH . '/layouts');
-define('ERRORS_PATH', BASE_PATH . '/errors');
-define('UPLOAD_PATH', BASE_PATH . '/uploads');
+define('BASE_PATH', realpath(__DIR__));
+define('HANDLERS_PATH', realpath(__DIR__ . '/handlers'));
+define('UTILS_PATH', realpath(__DIR__ . '/utils'));
+define('DATABASE_PATH', realpath(__DIR__ . '/database'));
+define('PAGES_PATH', realpath(__DIR__ . '/pages'));
+define('DUMMIES_PATH', realpath(__DIR__ . '/staticDatas/dummies'));
+define('COMPONENTS_PATH', realpath(__DIR__ . '/components'));
+define('TEMPLATES_PATH', realpath(__DIR__ . '/components/templates'));
+define('STATICDATAS_PATH', realpath(__DIR__ . '/staticDatas'));
+define('LAYOUTS_PATH', realpath(__DIR__ . '/layouts'));
+define('ERRORS_PATH', realpath(__DIR__ . '/errors'));
+define('UPLOADS_PATH', realpath(__DIR__ . '/uploads'));
 
 chdir(BASE_PATH);
+
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
