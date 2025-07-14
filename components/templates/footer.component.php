bbac23181dde1a85944382f373c3cc826ae9f91b
gabbycam98 <gabcamino1298@gmail.com>
2025-07-14 13:22:55 +0800

    feat: add header and footer component templates

---
+++ components/templates/footer.component.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+
+
+    <link rel="stylesheet" href="/assets/css/footer.css">
+
+
+</head>
+
+<body>
+    <footer>
+        <div class="footer-container">
+            <div class="footer-image">
+                <img src="/assets/img/travelez-green.png" alt="TravelEZ Logo">
+            </div>
+            <div class="copyright-info">
+                <p>© 2025 TravelEZ Philippines, All Rights Reserved.</p>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
