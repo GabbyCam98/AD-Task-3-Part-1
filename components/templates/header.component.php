1d14f0652450924e83c99dd07128d200b31f253a
gabbycam98 <gabcamino1298@gmail.com>
2025-07-14 15:23:09 +0800

    fix: simplify header structure by removing unnecessary HTML elements

--- components/templates/header.component.php
+++ components/templates/header.component.php
@@ -1,38 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+<header>
+    <div class="left-section">
+        <a href="../../index.php">
+            <img id="img-btn" src="../../assets/img/travelez-white.png" alt="travelEZ logo">
+        </a>
+    </div>
+    <div class="middle-section">
+    </div>
+    <div class="right-section">
+        <p class="date-box">
+            <?php
+            echo date("d F Y") . ", " . date("D");
+            ?>
+        </p>
+        <a href="/pages/aboutUs/index.php"><button class="nav-header">About Us</button></a>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Header</title>
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet">
-
-    <link rel="stylesheet" href="/assets/css/header.css">
-
-</head>
-
-<body>
-    <header>
-        <div class="left-section">
-            <a href="../../index.php">
-                <img src="../../assets/img/travelez-green.png" alt="travelEZ logo">
-            </a>
-        </div>
-        <div class="middle-section">
-        </div>
-        <div class="right-section">
-            <p class="date-box">
-                <?php
-                echo date("d F Y") . ", " . date("D");
-                ?>
-            </p>
-            <button class="nav-header">About Us</button>
-        </div>
-    </header>
-</body>
-
-</html>
+    </div>
+</header>
+<script src="/assets/js/header.js"></script>
