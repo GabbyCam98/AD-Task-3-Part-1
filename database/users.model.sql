d45c5a1656f1086fed829577fe47483b73594baf
gabbycam98 <gabcamino1298@gmail.com>
2025-07-27 11:00:47 +0800

    refactor: adjust indentation in users model SQL file

--- database/users.model.sql
+++ database/users.model.sql
@@ -1,4 +1,4 @@
-  CREATE TABLE IF NOT EXISTS public."users" (
+CREATE TABLE IF NOT EXISTS public."users" (
       id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
       first_name varchar(225) NOT NULL,
       middle_name varchar(225),
