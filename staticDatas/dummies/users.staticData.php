9e17145bffaed0049a6529e665fbb434710e56be
gabbycam98 <gabcamino1298@gmail.com>
2025-07-28 21:23:10 +0800

    feat: update user roles and add email addresses in static data

--- staticDatas/dummies/users.staticData.php
+++ staticDatas/dummies/users.staticData.php
@@ -1,5 +1,20 @@
 <?php
 
 return [
-    ['username' => 'john.smith', 'first_name' => 'John', 'last_name' => 'Smith', 'password' => 'p@ssW0rd', 'role' => 'designer'],
+    [
+        'username' => 'john.smith',
+        'first_name' => 'John',
+        'last_name' => 'Smith',
+        'password' => 'p@ssW0rd',
+        'role' => 'admin',
+        'email' => 'johnsmith@gmail.com'
+    ],
+    [
+        'username' => 'jane.doe',
+        'first_name' => 'Jane',
+        'last_name' => 'Doe',
+        'password' => 'p@ssW0rd123',
+        'role' => 'user',
+        'email' => 'jane.doe@example.com'
+    ]
 ];
