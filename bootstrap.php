7f7a0b04f320d58ff023dede0586a7adedcc0cf1
gabbycam98 <gabcamino1298@gmail.com>
2025-07-29 00:22:30 +0800

    fix: standardize path definitions in bootstrap file for consistency

--- bootstrap.php
+++ bootstrap.php
@@ -1,15 +1,14 @@
 <?php
 define('BASE_PATH', realpath(__DIR__));
-define('HANDLERS_PATH', realpath(__DIR__ . '/handlers'));
-define('UTILS_PATH', realpath(__DIR__ . '/utils'));
-define('DATABASE_PATH', realpath(__DIR__ . '/database'));
-define('DUMMIES_PATH', realpath(__DIR__ . '/staticDatas/dummies'));
-define('COMPONENTS_PATH', realpath(__DIR__ . '/components'));
-define('TEMPLATES_PATH', realpath(__DIR__ . '/components/templates'));
-define('STATICDATAS_PATH', realpath(__DIR__ . '/staticDatas'));
-define('LAYOUTS_PATH', realpath(__DIR__ . '/layouts'));
-define('ERRORS_PATH', realpath(__DIR__ . '/errors'));
-define('UPLOADS_PATH', realpath(__DIR__ . '/uploads'));
+define('HANDLERS_PATH', realpath(BASE_PATH . "/handlers"));
+define('UTILS_PATH', realpath(BASE_PATH . "/utils"));
+define('DATABASE_PATH', realpath(BASE_PATH . "/database"));
+define('DUMMIES_PATH', realpath(BASE_PATH . "/staticDatas/dummies"));
+define('TEMPLATES_PATH', realpath(BASE_PATH . '/components/templates'));
+define('STATICDATAS_PATH', realpath(BASE_PATH . '/staticDatas'));
+define('LAYOUTS_PATH', realpath(BASE_PATH . '/layouts'));
+define('ERRORS_PATH', realpath(BASE_PATH . '/errors'));
+define('UPLOADS_PATH', realpath(BASE_PATH . '/uploads'));
 
 chdir(BASE_PATH);
 
